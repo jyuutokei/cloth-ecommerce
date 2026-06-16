ca0986585aa2373baa5ac182249a1fb0b3dfb9b3
furantuturu <furantuturu@proton.me>
2026-06-16 21:55:40 +0800

    Enhance layout structure with minimum width for background layer and refined main section styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,10 +8,12 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
+{{-- <body class="text-[clamp(1rem,5vw,2.5rem)]"> --}}
+
 <body>
-    <div class="bg-layer"></div>
+    <div class="bg-layer min-w-118.75"></div>
     <x-nav />
-    <main>
+    <main class="mx-auto max-w-400 w-[90%] mt-10">
         {{ $slot }}
     </main>
 </body>
