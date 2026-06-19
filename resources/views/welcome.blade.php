d0277e79f10459d2a88bb3c8df754751bdae090a
furantuturu <furantuturu@proton.me>
2026-06-19 23:16:35 +0800

    Improve layout structure for better responsiveness on the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,14 @@
         Home
     </x-slot:title>
 
-    <div class="mt-20">
+    <div class="min-w-0 overflow-x-hidden w-full mt-20">
         <div>
             <p class="text-xl">Men</p>
             <p class="text-xl">Women</p>
             <p class="text-xl">Kids</p>
             <x-search-input class="mt-5" />
         </div>
-        <div class="md:grid grid-rows-[minmax(0,200px)_minmax(0,200px)] grid-cols-3 gap-4 mt-20">
+        <div class="min-w-0 md:grid grid-rows-[minmax(0,200px)_minmax(0,200px)] grid-cols-3 gap-4 mt-20">
             <div class="row-start-1 col-start-1">
                 <h1 class="font-extrabold text-6xl md:text-5xl lg:text-5xl xl:text-6xl">NEW <br> COLLECTION</h1>
                 <p class="text-lg mt-4">Discover the latest trends in fashion</p>
