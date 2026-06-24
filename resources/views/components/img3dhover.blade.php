bbd89155d72b165cc7d2782789f4f46f666c011f
furantuturu <furantuturu@proton.me>
2026-06-24 21:47:44 +0800

    Add 3D hover effect component for enhanced image interactivity

---
+++ resources/views/components/img3dhover.blade.php
@@ -0,0 +1,14 @@
+<div class="hover-3d w-full h-full">
+    <figure class="w-full">
+        {{ $slot }}
+    </figure>
+    <!-- 8 empty divs needed for the 3D effect -->
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
