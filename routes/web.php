81b3039431c77fbd61c8e7be2d33dd69f6ac5251
furantuturu <furantuturu@proton.me>
2026-06-24 21:54:52 +0800

    Add checkout route to web.php for checkout page navigation

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn () => view('welcome'))->name('home');
 Route::get('/products', fn () => view('products'))->name('products');
 Route::get('/product', fn () => view('product'))->name('product');
+Route::get('/checkout', fn () => view('checkout'))->name('checkout');
