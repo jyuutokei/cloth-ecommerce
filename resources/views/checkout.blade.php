a396033f31b5cea4eaf3b013b6df165647d4d7d4
furantuturu <furantuturu@proton.me>
2026-06-25 21:39:31 +0800

    Refactor checkout page layout: enhance structure, update delivery and payment sections, and improve order summary display

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -4,76 +4,61 @@
     </x-slot:title>
 
     <div class="min-w-0">
-        <h1 class="font-bold text-3xl">CHECKOUT</h1>
-        <div class="mt-8 flex justify-between items-start">
-            <div class="max-w-lg">
-                <div>
-                    <div class="border py-4 px-7">
-                        <h3 class="font-bold text-lg">Delivery Address</h3>
-                        <p class="text-sm text-gray-500">Enter your address to see delivery options</p>
-                        <div class="mt-5">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi sit minima
-                                culpa
-                                voluptatem architecto asperiores ducimus, cumque, officia autem dolorum enim ad quidem
-                                soluta
-                                praesentium perferendis? Aut, impedit ipsa?</p>
-                            <button aria-label="Change delivery address class=" btn border border-black rounded-none
-                                bg-transparent mt-3">Change</button>
-                        </div>
+        <h1 class="font-extrabold text-3xl">CHECKOUT</h1>
+        <div class="mt-8">
+            <div class="flex gap-5">
+                <div class="flex-1 border py-4 px-7">
+                    <h3 class="font-bold text-lg">Delivery Address</h3>
+                    <p class="text-sm text-gray-500">Enter your address to see delivery options</p>
+                    <div class="mt-5">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi sit minima
+                            culpa
+                            voluptatem architecto asperiores ducimus, cumque, officia autem dolorum enim ad quidem
+                        </p>
+                        <button aria-label="Change delivery address" class="btn border border-black rounded-none
+                                    bg-transparent mt-3">Change</button>
                     </div>
                 </div>
-                <div class="divider"></div>
-                <div class="mt-5">
-                    <div class="border py-4 px-7">
-                        <h3 class="font-bold text-lg">Payment Method</h3>
-                        <p class="text-sm text-gray-500">Choose your payment method</p>
-                        <div class="mt-5">
-                            <p>Cash on Delivery</p>
-                            <button aria-label="Change payment method"
-                                class="btn border border-black rounded-none bg-transparent mt-3">Change</button>
-                        </div>
+                <div class="divider lg:divider-horizontal"></div>
+                <div class="border py-4 px-7">
+                    <h3 class="font-bold text-lg">Payment Method</h3>
+                    <p class="font-light text-sm text-gray-500">Choose your payment method</p>
+                    <div class="mt-5">
+                        <p>Cash on Delivery</p>
+                        <button aria-label="Change payment method"
+                            class="btn border border-black rounded-none bg-transparent mt-8">Change</button>
                     </div>
                 </div>
             </div>
-            <div class="border p-6">
-                <h3>Your Order</h3>
-                <div>
-                    <div>
-                        <div>
-                            <figure class="max-w-27.5 aspect-3/4">
-                                <img class="w-full h-full object-cover"
-                                    src="{{ Vite::asset('resources/images/fashion-models/alameenng.webp') }}"
-                                    alt="Fashion model wearing new summer collection">
-                            </figure>
-                        </div>
-                        <div>
-                            <div>
-                                <h5>Basic Heavy T-shirt</h5>
-                                <p>Variant: </p>
-                            </div>
-                            <p>(1)</p>
-                        </div>
-                        <div>
-                            <button aria-label="Change order item"
-                                class="btn bg-transparent border border-black">Change</button>
-                            <p>$99</p>
-                        </div>
+        </div>
+        <div class="divider"></div>
+        <h3 class="font-bold text-2xl">Your Order</h3>
+        <div class="flex gap-5 mt-8">
+            <div class="grid grid-cols-[repeat(auto-fill,minmax(max(100%/3,300px),1fr))] gap-5 w-full">
+                <x-order-card />
+                <x-order-card />
+                <x-order-card />
+                <x-order-card />
+            </div>
+            <div class="divider lg:divider-horizontal"></div>
+            <div>
+                <div class="border p-6 w-sm sticky top-15">
+                    <div class="flex justify-between items-center mb-2">
+                        <p>Merchandise Subtotal:</p>
+                        <p class="font-bold">$99</p>
+                    </div>
+                    <div class="flex justify-between items-center">
+                        <p>Shipping Subtotal:</p>
+                        <p class="font-bold">$28</p>
+                    </div>
+                    <div class="divider"></div>
+                    <div class="flex justify-between items-center">
+                        <p class="font-bold">Total:</p>
+                        <p class="font-bold">$127</p>
                     </div>
                     <div>
-                        <div class="divider"></div>
-                        <div>
-                            <p class="flex-1">Merchandise Subtotal:</p>
-                            <p>$99</p>
-                        </div>
-                        <div>
-                            <p class="flex-1">Shipping Subtotal:</p>
-                            <p>$28</p>
-                        </div>
-                        <div class="divider"></div>
-                    </div>
-                    <div>
-                        <p class="flex-1">Total:</p>
-                        <p>$127</p>
+                        <button class="mt-7 w-full btn bg-transparent border border-black rounded-none">Place
+                            Order</button>
                     </div>
                 </div>
             </div>
