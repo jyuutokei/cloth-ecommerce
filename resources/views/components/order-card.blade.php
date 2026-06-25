81bbe90b73d480bbfda1c51e074a600a126269a8
furantuturu <furantuturu@proton.me>
2026-06-25 21:39:54 +0800

    Add order card component: create layout for product display with shipping cost

---
+++ resources/views/components/order-card.blade.php
@@ -0,0 +1,26 @@
+<div class="border p-6">
+    <div class="flex gap-4">
+        <div>
+            <figure class="max-w-31 aspect-3/4">
+                <img class="w-full h-full object-cover"
+                    src="{{ Vite::asset('resources/images/fashion-models/alameenng.webp') }}"
+                    alt="Fashion model wearing new summer collection">
+            </figure>
+        </div>
+        <div class="flex-1 flex flex-col justify-between">
+            <div>
+                <h5 class="text-lg font-bold truncate">Basic Heavy T-shirt</h5>
+                <p class="text-sm text-gray-500 mt-2">Variant: </p>
+            </div>
+            <div class="flex justify-between">
+                <p class="text-blue-500">(1)</p>
+                <p class="font-bold">$99</p>
+            </div>
+        </div>
+    </div>
+    <div class="divider"></div>
+    <div class="flex justify-between items-center">
+        <p>Shipping Cost:</p>
+        <p class="font-bold">$28</p>
+    </div>
+</div>
