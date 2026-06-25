a4fdbe519b07475993d34fd0eda1ed8cf87ef66d
furantuturu <furantuturu@proton.me>
2026-06-25 21:39:21 +0800

    Add cart view template with layout and title

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <x-slot:title>
+        Cart
+    </x-slot:title>
+
+    <div class="min-w-0">
+        <h1 class="font-extrabold text-3xl">Shopping Cart</h1>
+        <div class="divider"></div>
+    </div>
+</x-layout>
