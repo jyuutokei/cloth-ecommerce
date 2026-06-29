ed84d64b9f6cc15277c04bd42ce650b2ef87ae08
furantuturu <furantuturu@proton.me>
2026-06-29 23:32:11 +0800

    Add dropdown menu for mobile navigation in the navbar

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,18 @@
 <div class="navbar mt-10 max-w-400 w-[90%] mx-auto shadow-md px-5">
-    <div class="flex-1">
+    <div class="md:hidden flex-1 dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-15 w-15" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+            </svg>
+        </div>
+        <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a>Collections</a></li>
+            <li><a>New</a></li>
+        </ul>
+    </div>
+    <div class="hidden md:flex flex-1">
         <a href="{{ route("home") }}" class="btn btn-ghost text-xl">Home</a>
         <a href="#" class="btn btn-ghost text-xl">Collections</a>
         <a href="#" class="btn btn-ghost text-xl">New</a>
