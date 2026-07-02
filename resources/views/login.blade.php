8cfdc146598efd6f983cc8188189fbea86d94631
furantuturu <furantuturu@proton.me>
2026-07-02 21:20:40 +0800

    Added views for logging in and registering users

---
+++ resources/views/login.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <x-slot:title>
+        Login
+    </x-slot:title>
+
+    <div class="min-w-0 flex items-center justify-center min-h-[calc(100vh-15rem)]">
+        <fieldset class="fieldset bg-transparent w-xs border p-4">
+            <legend class="fieldset-legend">Login</legend>
+
+            <label class="label">Email</label>
+            <input type="email" class="input bg-altgray rounded-none" placeholder="Email" />
+
+            <label class="label">Password</label>
+            <input type="password" class="input bg-altgray rounded-none" placeholder="Password" />
+
+            <button class="btn bg-altgray mt-4 rounded-none">Login</button>
+        </fieldset>
+    </div>
+</x-layout>
