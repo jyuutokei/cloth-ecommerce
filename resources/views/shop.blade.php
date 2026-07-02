1967bd267d01816d5271cd40e31daee03e273d8c
furantuturu <furantuturu@proton.me>
2026-07-02 21:20:56 +0800

    Add filter button and toggle functionality to shop view

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -17,7 +17,19 @@
                     <x-search-input />
                 </div>
             </div>
-            <div class="mt-10 grid grid-cols-[repeat(auto-fill,minmax(max(100%/4,200px),1fr))] gap-5">
+            <div x-data="{ open: false }" class="md:hidden my-5">
+                <button class="btn bg-transparent group"
+                    @click="open = !open; $dispatch('toggle-filter', { open: open })">
+                    Filter
+                    <span x-show="!open">
+                        <x-icons.angle-arrow direction="right" />
+                    </span>
+                    <span x-show="open">
+                        <x-icons.angle-arrow direction="left" />
+                    </span>
+                </button>
+            </div>
+            <div class="md:mt-10 grid grid-cols-[repeat(auto-fill,minmax(max(100%/4,150px),1fr))] gap-5">
                 <x-card class="aspect-square" />
                 <x-card class="aspect-square" />
                 <x-card class="aspect-square" />
@@ -26,12 +38,12 @@
                 <x-card class="aspect-square" />
             </div>
         </div>
-        <div class="hidden md:flex flex-col">
+        <div x-data="{ open: false }" @toggle-filter.window="open = $event.detail.open"
+            x-transition:enter="transition ease-out duration-150" x-transition:enter-start="opacity-0 -translate-x-10"
+            x-transition:enter-end="opacity-100 translate-x-0" x-transition:leave="transition ease-in duration-150"
+            x-transition:leave-start="opacity-100 translate-x-0" x-transition:leave-end="opacity-0 -translate-x-10"
+            x-show="open" class="flex flex-col mr-10 md:mr-0 md:!flex">
             <h3 class="font-bold text-2xl mt-25">Filters</h3>
-            <div>
-                <h3 class="font-bold text-xl mt-4 mb-1">Size</h3>
-                <x-buttons.size-tabs />
-            </div>
             <div class="divider w-full"></div>
             <div>
                 <x-accordion>Availability</x-accordion>
